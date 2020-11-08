0078caa7a9a3ff173598e3b12e3dcea0427eeed1
Akash Gupta <akashgp9@gmail.com>
2020-11-08 20:41:01 +0530

    📦 Components

---
+++ src/components/ReadyToTalk/ReadyToTalk.tsx
@@ -0,0 +1,62 @@
+import styles from "./readyToTalk.module.css";
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      center: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+export default function ReadyToTalk() {
+  return (
+    <>
+      <section className={styles.ready_to_talk}>
+        <div className="container">
+          <h3>Ready to talk?</h3>
+          <p>Google Assistant can answer most of your questions.</p>
+          <div className={styles.icon}>
+            <div
+              className={styles.bar}
+              style={{ backgroundColor: "#3498db", marginLeft: -60 }}
+            ></div>
+            <div
+              className={styles.bar}
+              style={{ backgroundColor: "#e74c3c", marginLeft: -20 }}
+            ></div>
+            <div
+              className={styles.bar}
+              style={{ backgroundColor: "#f1c40f", marginLeft: 20 }}
+            ></div>
+            <div
+              className={styles.bar}
+              style={{ backgroundColor: "#27ae60", marginLeft: 60 }}
+            ></div>
+          </div>
+
+          <a
+            href="https://assistant.google.com/services/invoke/uid/0000006c15659a07"
+            target="_blank"
+            className={`btn ${styles.talkDSC}`}
+            style={{ color: "grey", backgroundColor: "white" }}
+          >
+            Talk to DSC KIIT
+          </a>
+          <br />
+          <br />
+
+          <center>
+            <a
+              href="#contact"
+              className={`btn ${styles.contactAssist}`}
+              style={{ color: "grey", backgroundColor: "white" }}
+            >
+              Contact
+            </a>
+          </center>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Footer from "../components/Footer/Footer";
+// import ReadyToTalk from "../components/ReadyToTalk/ReadyToTalk";
+// import TeamIntro from "../components/Intro/Intro";
+
+import Navbar from "../components/Navbar/Navbar";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        />
+        <link rel="stylesheet" href="./index.css" />
+      </Head>
+
+      <Navbar />
+      {/* <TeamIntro />
+      <ReadyToTalk /> */}
+      <Footer />
+    </>
+  );
+}
