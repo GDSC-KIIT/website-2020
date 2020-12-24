16a52e60f5e9b14e755a7141a6601bf6742a7903
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-12-24 10:51:28 +0530

    commited files inside cms dir

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b6611ecdc5ebf668cc72deb8c8f45eb3'),
+    },
+  },
+});
