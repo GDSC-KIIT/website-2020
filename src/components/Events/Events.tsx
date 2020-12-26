3095fabaca3b5f361d08d31d345584f7be13d2b9
yashvi2001 <yamyashvi201@gmail.com>
2020-12-26 15:44:30 +0530

    added events page

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -8,63 +8,20 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Grid from "@material-ui/core/Grid";
+import Grid from '@material-ui/core/Grid';
 
 import EventIcon from "@material-ui/icons/Event";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary
-  },
-
-  button: {
-    backgroundColor: "#746B6B",
-    color: "white"
-  },
-  rootCard: {
-    maxWidth: 350,
-    height: 395
-  },
-  rootCardMobile: {
-    width: "auto",
-    height: 420
+    maxWidth: 340,
+    marginLeft:50,
+    marginButtom:50
   },
   media: {
-    height: 230
+    height: 200,
   },
-  grid: {
-    height: 550
-  },
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 400,
-    margin: "auto"
-  },
-  paperModal: {
-    backgroundColor: theme.palette.background.paper,
-    // border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
-  },
-  cardAction: {
-    "&:focus": {
-      backgroundColor: "#ffffff"
-    }
-  },
-  cardActionDark: {
-    "&:focus": {
-      backgroundColor: "#202020"
-    }
-  }
-}));
-
+});
 
 export default function Events() {
     const classes = useStyles();
@@ -83,45 +40,36 @@ export default function Events() {
         Stay tuned for future events!
             </p>
           </div>
-           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
-          <Card className={classes.root}>
-           <CardActionArea>
-             <CardMedia
-               className={classes.media}
-               image=""
-               title="Event 1"
-             />
-             <CardContent>
-             <Grid
-                         container
-                         direction="column"
-                         justify="space-between"
-                       >
-               <Typography gutterBottom variant="h5" component="h2">
-                Event 1
-               </Typography>
-               <Grid style={{ padding: 3 }}>
-                              <Typography>
-                                <EventIcon /> DD/MM/YY
-                              </Typography>
-                            </Grid>
-                  </Grid>
-               <Typography variant="body2" color="textSecondary" component="p">
-          Events are a great way to share knowledge and indulge in great discussions with your peers.
-               </Typography>
-             </CardContent>
-           </CardActionArea>
-           <CardActions>
+          <Grid item>
+              <Card className={classes.root}>
+                <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image=""
+                  title="DSC KIIT Events"
+                />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Event
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                    description about the event
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <Grid style={{ padding: 3 }}>
+                            <Typography>
+                              <EventIcon />DD/MM/YY
+                            </Typography>
+                          </Grid>
+                <CardActions>
 
-             <Button size="small" color="primary">
-               Learn More
-             </Button>
-           </CardActions>
-         </Card>
-
-</Grid>
-</Grid>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
     </>
   );
 }
