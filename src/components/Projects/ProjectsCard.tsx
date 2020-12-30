80e4c6e1fafd51ec702efd0fd7949f7378512fad
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2020-12-31 03:29:02 +0530

    Added CMS in Projects Page

--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -10,6 +10,14 @@ import {
 	Button,
 } from '@material-ui/core';
 import { GitHub as GitHubIcon, Person as PersonIcon } from '@material-ui/icons';
+import { getStrapiMedia } from '../../lib/media';
+declare global {
+	namespace JSX {
+		interface IntrinsicElements {
+			center: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+		}
+	}
+}
 
 const useStyles = makeStyles({
 	root: {
@@ -20,44 +28,54 @@ const useStyles = makeStyles({
 	},
 });
 
-export default function ProjectsCard() {
+export default function ProjectsCard({ projectInfo, displayHeader }: any) {
+	const imageUrl = getStrapiMedia(projectInfo.image_path);
 	const classes = useStyles();
 
 	return (
-		<Card className={classes.root} raised={true}>
-			<CardActionArea>
-				<CardMedia
-					className={classes.media}
-					image="/projectslogo.png"
-					title="DSC KIIT projects"
-				/>
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="h2">
-						Project
-					</Typography>
-					<div
-						style={{
-							display: 'flex',
-							justifyContent: 'flex-start',
-							alignItems: 'center',
-							marginTop: 10,
-						}}>
-						<PersonIcon style={{ marginRight: 10 }} />
+		<>
+			{console.log(projectInfo, displayHeader)}
+			{/* {displayHeader ? (
+				<center className={styles.techDesc}>
+					<h3>{positionHeading[memberInfo.order]}</h3>
+				</center>
+			) : (
+				''
+			)} */}
+			<Card className={classes.root} raised={true}>
+				<CardActionArea>
+					<CardMedia
+						className={classes.media}
+						image="/projectslogo.png"
+						title="DSC KIIT projects"
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{projectInfo.name}
+						</Typography>
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'flex-start',
+								alignItems: 'center',
+								marginTop: 10,
+							}}>
+							<PersonIcon style={{ marginRight: 10 }} />
 
-						<Typography>Project Members Name</Typography>
-					</div>
-				</CardContent>
-			</CardActionArea>
-			<CardActions>
-				<Button
-					variant="outlined"
-					color="primary"
-					startIcon={<GitHubIcon />}
-					href="https://github.com
-				">
-					GitHub
-				</Button>
-			</CardActions>
-		</Card>
+							<Typography>Project Members Name</Typography>
+						</div>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Button
+						variant="outlined"
+						color="primary"
+						startIcon={<GitHubIcon />}
+						href="https://github.com">
+						GitHub
+					</Button>
+				</CardActions>
+			</Card>
+		</>
 	);
 }
