526c2d026184e740376684beea127089bc6ab88d
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-06 00:12:38 +0530

    show logged in user in the navbar

--- src/components/AuthProvider/index.tsx
+++ src/components/AuthProvider/index.tsx
@@ -1,14 +1,32 @@
-import { Typography } from '@material-ui/core';
-import Link from 'next/link';
+import useUser from '@/hooks/useUser';
+
+import { Link, CircularProgress, Typography } from '@material-ui/core';
+
+interface IPropsCurrentUser {
+	href: string;
+	text: string;
+}
+
+const CurrentUser = ({ href, text }: IPropsCurrentUser) => (
+	<Link href={href}>
+		<Typography variant="body2" style={{ cursor: 'pointer' }}>
+			{text}
+		</Typography>
+	</Link>
+);
+
 export function GoogleAuthLogin() {
-	// TODO: Add styling
+	const { loading, user } = useUser();
+
 	return (
 		<div>
-			<Link href="http://localhost:9000/connect/google">
-				<Typography variant="body2" style={{ cursor: 'pointer' }}>
-					SignIn
-				</Typography>
-			</Link>
+			{loading ? (
+				<CircularProgress />
+			) : user ? (
+				<CurrentUser href="" text={`Hi, ${user.username}`} />
+			) : (
+				<CurrentUser href="http://localhost:9000/connect/google" text="Sign In" />
+			)}
 		</div>
 	);
 }
