b3fd7174e022bcbbfe82d286754175318979773f
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-07 14:43:00 +0530

    find and update score of the user

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -2,10 +2,11 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjA5OTYzNTY0LCJleHAiOjE2MTI1NTU1NjR9.NNCSLFoP1uo7fzMZgS2q5sZiGOF8kc3KQmwWDWZLD_4';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMDA3NTc5LCJleHAiOjE2MTI1OTk1Nzl9.khmwxMoq1HCsMAycrvLHpicVyWyAWfIk_NXy2W16fAQ';
 
 export default function Testing() {
 	const [data, setData] = useState('loading');
+
 	useEffect(() => {
 		const fetchCall = async () => {
 			await axios
