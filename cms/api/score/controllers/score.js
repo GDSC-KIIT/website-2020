126b4e8bfba91d0578d9473927dc5e948c1d3548
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-07 14:43:00 +0530

    find and update score of the user

--- cms/api/score/controllers/score.js
+++ cms/api/score/controllers/score.js
@@ -5,12 +5,59 @@
  * to customize this controller
  */
 
+/**
+ * contains the information about the authenticated user
+ * @typedef user
+ * @type {object}
+ * @property {number} id
+ * @property {string} username
+ * @property {string} provider google should be the selected auth provider
+ * @property {string} email
+ * @property {number|null} score
+ */
+
+/**
+ * interested fields to update in the score model
+ * @typedef score
+ * @type {object}
+ * @property {number} id
+ * @property {number} points
+ */
+
+const INCREMENT = 10;
+
 module.exports = {
 	async find(ctx) {
-		const users = await strapi
-			.query('user', 'user-permissions')
-			.find({ username: gamersinstinct7 });
-		console.log('got the users as', users);
-		return users;
+		/**
+		 * @type {user}
+		 */
+		const user = ctx.state.user;
+
+		if (!user) {
+			ctx.send('no user found');
+		}
+
+		if (user.score) {
+			// update the user's score
+			const foundScoresArray = await strapi.services.score.find({ id: user.score });
+
+			/**
+			 * @type {score}
+			 */
+			const foundScore = foundScoresArray[0];
+			// const newScore = await strapi.sever
+
+			const upd = await strapi.services.score.update(
+				{ id: foundScore.id },
+				{ points: foundScore.points + INCREMENT }
+			);
+			console.log('tweaked changes', upd);
+		} else {
+			// create a new score for the user
+			console.log('no one foudn');
+			ctx.send('create a new score for the user');
+		}
+
+		ctx.send('done');
 	},
 };
