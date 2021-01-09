8201111b194a4a3c74effd2e4e605c784981ab37
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-09 12:41:07 +0530

    configured image upload to firebase

---
+++ cms/providers/strapi-provider-upload-firebase/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../../firebase-env.json');
+
+module.exports = {
+	init(providerOptions) {
+		console.log('the provided opitons were', providerOptions);
+		admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount),
+			storageBucket: providerOptions.storageBucket,
+		});
+
+		const bucket = admin.storage().bucket();
+
+		return {
+			upload(file) {
+				return new Promise((resolve, reject) => {
+					const path = file.path ? `${file.path}/` : '';
+					const filename = `${path}${file.hash}${file.ext}`;
+					const buff = Buffer.from(file.buffer, 'binary');
+					const remoteFile = bucket.file(filename);
+					remoteFile.save(
+						buff,
+						{
+							resumable: false,
+							contentType: file.mime,
+							public: true,
+						},
+						(err) => {
+							if (err) {
+								reject(err);
+							}
+							file.url = `https://storage.googleapis.com/${providerOptions.storageBucket}/${filename}`;
+							resolve();
+						}
+					);
+				});
+			},
+			delete(file) {
+				return new Promise((resolve, reject) => {
+					const path = file.path ? `${file.path}/` : '';
+					const filename = `${path}${file.hash}${file.ext}`;
+					const remoteFile = bucket.file(filename);
+					remoteFile.delete((err, _) => {
+						if (err) {
+							return reject(err);
+						}
+						resolve();
+					});
+				});
+			},
+		};
+	},
+};
