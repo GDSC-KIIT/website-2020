eeab71df516350bfbb992d7d2382aca7ea8d4c2e
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-13 22:27:22 +0530

    added todo workflow

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-	errorOnDeprecated: true,
 	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '<rootDir>/tests/**/?(*.)+(spec|test).[jt]s?(x)'],
 	setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+	errorOnDeprecated: true,
 	testTimeout: 7000,
 	verbose: false,
 	moduleNameMapper: {
