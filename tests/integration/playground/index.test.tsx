e972778ec555498954fdb705575d54dbb516efb1
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-14 23:12:47 +0530

    added custom renders for server stylesheets

--- tests/integration/playground/index.test.tsx
+++ tests/integration/playground/index.test.tsx
@@ -1,11 +1,15 @@
 import { render } from '@testing-library/react';
 import Question from '@/playground/components/question';
 
+import { ssrRender } from '../../utils';
+
 it('renders and has radio buttons', () => {
 	// changed the render to customRender to support server stylesheets
-	const { container } = render(<Question />, { hydrate: false });
 	// console.info('the container here is', container.innerHTML);
 	// expect(container.innerHTML).tomatch
+	const { container } = ssrRender(<Question />);
+
+	console.log(container.innerHTML);
 
 	expect(2 + 2).toEqual(4);
 });
---
+++ tests/utils/index.ts
@@ -0,0 +1,1 @@
+export { ssrRender } from './ui';
