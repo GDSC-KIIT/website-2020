838256e37d41a83ab03573a386f0fd3ee345956c
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-15 00:00:02 +0530

    test for user not logged in working

--- tests/integration/playground/index.test.tsx
+++ tests/integration/playground/index.test.tsx
@@ -1,15 +1,11 @@
 import { render } from '@testing-library/react';
 import Question from '@/playground/components/question';
 
-import { ssrRender } from '../../utils';
-
 it('renders and has radio buttons', () => {
 	// changed the render to customRender to support server stylesheets
 	// console.info('the container here is', container.innerHTML);
 	// expect(container.innerHTML).tomatch
-	const { container } = ssrRender(<Question />);
-
-	console.log(container.innerHTML);
+	const { container } = render(<Question />);
 
 	expect(2 + 2).toEqual(4);
 });
