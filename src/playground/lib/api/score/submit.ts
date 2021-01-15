b1a75fc0c6e4b7d651d1b4fac30d36fcad9ec0f2
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-16 02:05:32 +0530

    refactor playground submission

--- src/playground/lib/api/score/submit.ts
+++ src/playground/lib/api/score/submit.ts
@@ -8,23 +8,21 @@ const baseURL: string = process.env.BACKEND_URL || 'http://localhost:9000';
 export default async function submitAnswer(qid: number, ans: number, config?: AxiosRequestConfig) {
 	const authToken = await getSessionAuthToken();
 
-	console.info('qid amd ans', qid, ans);
-
 	return axios({
 		url: baseURL + '/scores',
 		method: 'POST',
 		headers: { Authorization: `Bearer ${authToken}` },
+		data: { qid, ans },
 		...config,
 	})
 		.then((response) => response.data)
 		.then((data: SubmitReponseType) => data)
 		.catch((err: AxiosError) => {
-			if (err.response?.headers === 403) {
+			if (err.response?.status === 403) {
 				throw new Error('you are not logged in!');
-			} else if (err.response?.data.message) {
+			} else if (err.response && err.response.data && 'message' in err.response.data) {
 				throw new Error(err.response.data.message);
 			}
-			console.log(err, 'during submittimg the question');
 			throw new Error('an unknown error occurred');
 		});
 }
