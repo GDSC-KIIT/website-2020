6305c3d47f28e3ca82fa86a1d72c3bb6e4c9f7e8
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-17 01:21:20 +0530

    fixed errors during nextjs build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import NextDocument from 'next/document';
+import { FC, Fragment } from 'react';
+import NextDocument, { DocumentContext } from 'next/document';
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
 
 export default class Document extends NextDocument {
-	static async getInitialProps(ctx) {
+	static async getInitialProps(ctx: DocumentContext) {
 		const materialUiSheets = new MaterialUiServerStyleSheets();
 		const originalRenderPage = ctx.renderPage;
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) => materialUiSheets.collect(<App {...props} />),
-				});
-			const initialProps = await NextDocument.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: [
-					<React.Fragment key="styles">
-						{initialProps.styles}
-						{materialUiSheets.getStyleElement()}
-					</React.Fragment>,
-				],
-			};
-		} catch (e) {
-			console.log(e);
-		}
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App: FC) => (props) => materialUiSheets.collect(<App {...props} />),
+			});
+		const initialProps = await NextDocument.getInitialProps(ctx);
+		return {
+			...initialProps,
+			styles: [
+				<Fragment key="styles">
+					{initialProps.styles}
+					{materialUiSheets.getStyleElement()}
+				</Fragment>,
+			],
+		};
 	}
 }
