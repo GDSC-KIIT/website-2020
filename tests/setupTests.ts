cb03b8fdb52f07ec9574639595ff345db10ab13c
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-16 18:41:59 +0530

    added test for testing the display of question

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -2,4 +2,6 @@ import '@testing-library/jest-dom/extend-expect';
 
 import { createUser } from './utils/api';
 
-createUser();
+// TODO move this function into each integration test
+//  also fix the integration tests by doing `await waitFor` in all of the tests
+// createUser();
--- tests/unit/playground/singleQuestion.test.tsx
+++ tests/unit/playground/singleQuestion.test.tsx
@@ -1,27 +1,54 @@
-import { render, waitFor, act } from '@testing-library/react';
+import { cleanup, render, waitFor, act } from '@testing-library/react';
 import { logger, testUtils } from '../../utils';
 import { mocked } from 'ts-jest/utils';
 
+import mockAxios, { AxiosRequestConfig } from 'axios';
 import Question from '@/playground/components/question';
 const useRouter = jest.spyOn(require('next/router'), 'useRouter');
-import { getUserInfo } from '@/lib/user/getUser';
 
-jest.mock('@/lib/user/session');
-jest.mock('@/lib/user/getUser', () => ({
-	__esModule: true,
-	getUserInfo: jest.fn(),
-}));
+beforeEach(() => {
+	useRouter.mockImplementation(() => ({ query: { play: '10' } }));
+
+	mocked(mockAxios).mockImplementation(
+		(config: any): Promise<any> => {
+			/**config will be an AxiosRequestConfig,
+			 * This happens when the default axios function is being called
+			 *
+			 * Also if an axios instance was called, then the instance will point to the mocked axios default function
+			 * The baseURL or other properites declared in the axios created instance will be invisible
+			 * the Promise.resolve has to be made on the remaining config
+			 */
+			const rqConfig: AxiosRequestConfig = config;
+
+			if (rqConfig.url === '/quizzes/10') {
+				logger('info', 'quizzes/10 was called');
+				return Promise.resolve({ data: testUtils.mockedQuestion1 });
+			}
+			logger('info', 'other method was called');
+			console.info(rqConfig.url);
+			return Promise.resolve();
+		}
+	);
+
+	mocked(mockAxios.get).mockImplementation((url, config) => {
+		if (url === 'http://localhost:9000/users/me') {
+			logger('info', 'users/me was called');
+			return Promise.resolve({ data: testUtils.mockUser });
+		} else if (url === '/api/session') {
+			return Promise.resolve({ data: { auth_token: 'user-jwt-token' } });
+		} else if (url === 'http://localhost:9000/quizzes/10') {
+			logger('info', 'the question was fetched');
+			return Promise.resolve(testUtils.mockedQuestion1);
+		}
+		return Promise.resolve();
+	});
+});
+
+afterEach(() => {
+	cleanup();
+});
 
 describe('next router is called', () => {
-	beforeAll(() => {
-		useRouter.mockImplementation(() => ({ query: { play: '10' } }));
-		logger(
-			'info',
-			'console.log statements',
-			'we have not yet mocked the other functionalities'
-		);
-	});
-
 	it('the useRouter function is called', () => {
 		render(<Question />);
 		act(() => {
@@ -31,14 +58,11 @@ describe('next router is called', () => {
 	});
 });
 
-describe('user is taken into account', () => {
-	beforeAll(() => {
-		mocked(getUserInfo).mockResolvedValue(testUtils.mockUser);
-	});
-	it('the user info is called', () => {
-		render(<Question />);
-		act(() => {
-			expect(getUserInfo).toHaveBeenCalled();
+describe('question is visible', () => {
+	it('option 1 is visible', async () => {
+		const { getByText } = render(<Question />);
+		await waitFor(() => {
+			expect(getByText(testUtils.mockedQuestion1.option_1)).toBeInTheDocument();
 		});
 	});
 });
