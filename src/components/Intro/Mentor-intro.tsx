01cc0833a2b535d57c6307b5a6d934bdcbfbd975
Akash Gupta <akashgp9@gmail.com>
2021-01-18 21:00:49 +0530

    updated Mentors page
    Signed-off-by: Akash Gupta <akashgp9@gmail.com>

--- src/components/Intro/Mentor-intro.tsx
+++ src/components/Intro/Mentor-intro.tsx
@@ -1,30 +1,48 @@
 import styles from './intro.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		marginTop: '3rem',
+		marginBottom: '3rem',
+		paddingLeft: '15px',
+		paddingRight: '15px',
+		maxWidth: '1500px',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+	},
+}));
+
 export default function Intro() {
+	const classes = useStyles();
 	return (
 		<>
-			<div className={`container my-5`}>
-				<div className="row">
-					<div className={`col-12 col-lg-6 ${styles.welcome_team}  pb-5`}>
-						<h1 className={`${styles.header}`}>MEET OUR MENTORS</h1>
+			<Grid container className={classes.root}>
+				<Grid item md={6} xs={12} className={styles.welcome_team}>
+					<h1 className={`${styles.header}`}>MEET OUR MENTORS</h1>
 
-						<p className={styles.dsc_description}>
-							Google collaborates with university students who are enthusiastic about
-							growing developer communities and supports them with commencing student
-							clubs on their campuses. Developer Student Clubs is a program that
-							recognizes and supports university students who are excited about
-							growing developer communities that cultivate learning, sharing, and
-							collaboration.
-						</p>
-					</div>
-					<div className={`col-lg-6 ${styles.introImg}`}>
-						<img
-							src="https://image.freepik.com/free-vector/internship-job-illustration_52683-50829.jpg"
-							className={styles.intro_image}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className={`${styles.our_team} mt-5`}>
+					<p className={styles.dsc_description}>
+						Google collaborates with university students who are enthusiastic about
+						growing developer communities and supports them with commencing student
+						clubs on their campuses. Developer Student Clubs is a program that
+						recognizes and supports university students who are excited about growing
+						developer communities that cultivate learning, sharing, and collaboration.
+					</p>
+				</Grid>
+				<Grid item md={6} className={styles.introImg}>
+					<img
+						src="https://image.freepik.com/free-vector/internship-job-illustration_52683-50829.jpg"
+						className={styles.intro_image}
+					/>
+				</Grid>
+			</Grid>
+
+			<div
+				className={`${styles.our_team}`}
+				style={{
+					marginTop: '3rem',
+				}}>
 				<h1 className={styles.header}>Mentors</h1>
 			</div>
 
@@ -35,7 +53,13 @@ export default function Intro() {
 				<div className={`${styles.green} ${styles.ball}`}></div>
 			</div>
 
-			<h3 style={{ textAlign: 'center', color: '#cfcece' }} className="mt-3 mb-5">
+			<h3
+				style={{
+					textAlign: 'center',
+					color: '#cfcece',
+					marginTop: '1rem',
+					marginBottom: '3rem',
+				}}>
 				Under the patronage of
 			</h3>
 		</>
