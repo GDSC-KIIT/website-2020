a7d2ac7409374352a634abcce6d77577e4bb5624
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-20 03:20:28 +0530

    try find controller in season-score

--- cms/api/season-score/controllers/season-score.js
+++ cms/api/season-score/controllers/season-score.js
@@ -5,4 +5,16 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+	async find() {
+		// both are async
+		const b = await strapi.query('season-score').find();
+		const a = strapi.services['season-score'].find();
+
+		const seasonScore = {
+			limit: b[0].limit,
+			id: b[0].id,
+		};
+		return seasonScore;
+	},
+};
