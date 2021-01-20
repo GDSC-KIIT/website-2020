8f9120d76f07b78ca1e9ea735de07d4c4d24e141
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2021-01-20 11:51:22 +0530

    Projects CMS and Images Updated

--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -41,8 +41,8 @@ export default function ProjectsCard({ projectInfo, displayHeader }: any) {
 					<CardActionArea>
 						<CardMedia
 							className={classes.media}
-							// image={imageUrl}
-							image="/projectslogo.png"
+							image={imageUrl}
+							// image="/projectslogo.png"
 							title="DSC KIIT projects"
 						/>
 						<CardContent>
@@ -57,12 +57,7 @@ export default function ProjectsCard({ projectInfo, displayHeader }: any) {
 									marginTop: 10,
 								}}>
 								<PersonIcon style={{ marginRight: 10 }} />
-								{/* 
-<<<<<<< HEAD
 								<Typography>{projectInfo.members.names}</Typography>
-======= */}
-								<Typography>Project Members Name</Typography>
-								{/* >>>>>>> 297fce798fa665e13d5c5e4819c07f6a0b2d5f34 */}
 							</div>
 						</CardContent>
 					</CardActionArea>
