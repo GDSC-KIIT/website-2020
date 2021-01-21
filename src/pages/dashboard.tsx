c2ce5dc6f7584c5cd6398ae08985a3911d939d5c
yashvi2001 <yamyashvi201@gmail.com>
2021-01-21 16:22:05 +0530

    added user dashboard for badges

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import Dashboard from '@/components/UserDashboard';
 import ReadyToTalk from '@/components/ReadyToTalk';
 import Layout from '@/components/Layout';
 
-export default function Mentor() {
+export default function Profile() {
 	return (
 		<>
 			<Layout pageName="Dashboard">
