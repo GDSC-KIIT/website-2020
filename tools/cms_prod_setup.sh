667054cc9ae4718c299c9123e3e2d0b4b7e3eb78
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-03-06 12:04:27 +0530

    add script production setup of cms

--- tools/cms_prod_setup.sh
+++ tools/cms_prod_setup.sh
@@ -1,5 +1,37 @@
-rm -rf ../LOGS/
+# in the root folder
 
-mkdir ../LOGS/
+rm -rfv LOGS/
 
-npm prune --production
+mkdir LOGS/ # create the LOGS folder in the project root
+
+# in the cms folder
+
+cd cms/
+
+rm -rfv node_modules/ # remove existing node_modules if any
+
+export SKIP_DECR=TRUE # first skip to ensure smooth installation of node_modules
+
+npm ci
+
+npm install # in case any dependency is missed
+
+export SKIP_DECR=FALSE
+
+printf "\n\n=================================================================\n\n"
+echo "Make sure you have set the PROD_DECR TO THE CORRECT SECRET"
+
+export PROD_DECR=#correct env secret
+
+echo "PROD_DECR is $PROD_DECR"
+printf "\n\n=================================================================\n\n"
+
+export NODE_ENV=production
+
+npm run postinstall
+
+printf "\n\n=================================================================\n\n"
+echo "skipping build. Make sure you have the required files already built."
+printf "\n\n=================================================================\n\n"
+
+npm prune --production # remove the dev dependencies
