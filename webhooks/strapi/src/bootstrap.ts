b02bffa6eb959e775054b25bb6a90d95423d61e3
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-03-06 13:08:14 +0530

    correct webhook for heroku deploy

--- webhooks/strapi/src/bootstrap.ts
+++ webhooks/strapi/src/bootstrap.ts
@@ -1,17 +1,11 @@
-import {
-	server as HapiServer,
-	Server as IHapiServer,
-	ServerRoute as IServerRoute,
-} from '@hapi/hapi';
+import { server as HapiServer, ServerRoute as IServerRoute } from '@hapi/hapi';
 
-const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || '9001';
 const DEBUG_LEVEL = process.env.NODE_ENV === 'development' ? ['*'] : false;
 
 const server = HapiServer({
 	debug: { request: DEBUG_LEVEL },
 	port: PORT,
-	host: HOST,
 });
 
 export const indexRoute: IServerRoute = {
