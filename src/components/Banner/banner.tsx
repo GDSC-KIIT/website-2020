bbe6eada0e939202dc3f3b702bb935b0ea431e08
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-03-26 23:32:24 +0530

    make the overlay less dark

--- src/components/Banner/banner.tsx
+++ src/components/Banner/banner.tsx
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
 		bottom: 0,
 		right: 0,
 		left: 0,
-		backgroundColor: 'rgba(0,0,0,.3)',
+		backgroundColor: 'rgba(0,0,0,.15)',
 	},
 	mainFeaturedPostContent: {
 		position: 'relative',
