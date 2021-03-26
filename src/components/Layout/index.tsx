0270f0ecbf04e48f297f5310d30f0fac22d84966
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-03-26 21:54:58 +0530

    added favicons and webmanifest

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -23,6 +23,8 @@ export default function Layout({ children, pageName }: IProps) {
 		<>
 			<Head key="layout">
 				<title>DSC KIIT | {pageName.toUpperCase()}</title>
+				<Favicons />
+				<link rel="manifest" href="/site.webmanifest" crossOrigin="use-credentials" />
 			</Head>
 			<ThemeProvider theme={theme}>
 				<Navbar />
@@ -33,6 +35,17 @@ export default function Layout({ children, pageName }: IProps) {
 					<Footer />
 				</div>
 			</ThemeProvider>
+		</>
+	);
+}
+
+function Favicons() {
+	return (
+		<>
+			<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+			<link rel="icon" type="image/png" href="/favicons/favicon.png" />
 		</>
 	);
 }
