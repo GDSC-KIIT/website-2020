6dd3aa8b4094fd79f6ee250515d87b1e7fb8205c
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-04-11 14:06:45 +0530

    post endpoints tests

--- cms/tests/unauthEndpoints.test.ts
+++ cms/tests/unauthEndpoints.test.ts
@@ -2,8 +2,10 @@ import request from 'supertest';
 
 import { setUpServer, tearDownServer } from './lib';
 
-beforeAll(() => {
-	return setUpServer();
+let strapiServer: any;
+beforeAll(async () => {
+	await setUpServer();
+	strapiServer = (strapi as any).server;
 }, 15000);
 
 afterAll((done) => {
@@ -12,25 +14,28 @@ afterAll((done) => {
 });
 
 describe('[BANNER]', () => {
-	it('should return 200 response', async () => {
-		await request((strapi as any).server)
-			.get('/banners')
-			.expect(200);
+	it('should return 200 response on GET endpoints', async () => {
+		await request(strapiServer).get('/banners').expect(200);
+	});
+	it('should return 400 response on POST endpoints', async () => {
+		await request(strapiServer).post('/banners').send({}).expect(403);
 	});
 });
 
 describe('[MEMBERS]', () => {
-	it('should return 200 response', async () => {
-		await request((strapi as any).server)
-			.get('/members')
-			.expect(200);
+	it('should return 200 response on GET endpoints', async () => {
+		await request(strapiServer).get('/members').expect(200);
+	});
+	it('should return 400 response on POST endpoints', async () => {
+		await request(strapiServer).post('/members').send({}).expect(403);
 	});
 });
 
 describe('[PROJECTS]', () => {
-	it('should return 200 response', async () => {
-		await request((strapi as any).server)
-			.get('/projects')
-			.expect(200);
+	it('should return 200 response on GET endpoints', async () => {
+		await request(strapiServer).get('/projects').expect(200);
+	});
+	it('should return 400 response on POST endpoints', async () => {
+		await request(strapiServer).post('/projects').send({}).expect(403);
 	});
 });
