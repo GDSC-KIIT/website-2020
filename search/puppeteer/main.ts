e3a4480e4ef3cf2440587642c41b40e35b20f21f
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-04-16 01:47:14 +0530

    create a search index file after extraction

--- search/puppeteer/main.ts
+++ search/puppeteer/main.ts
@@ -1,16 +1,19 @@
 import puppeteer from 'puppeteer';
+import Fuse from 'fuse.js';
+import fs from 'fs';
 
 import { baseURL, isHeadless, searchablePages, searchableSelector } from './constants';
-
 interface IScrappedData {
 	pageName: string;
 	name: string;
 	text: string;
 }
 
-async function getSearchableInPage(page: puppeteer.Page, currentSearchablePage: string) {
+async function getSearchableInPage(
+	page: puppeteer.Page,
+	currentSearchablePage: string
+): Promise<IScrappedData[]> {
 	await page.goto(baseURL + currentSearchablePage);
-
 	const foundContent = await page.$$eval(searchableSelector, (scontents, currentSearchablePage) =>
 		scontents.map((sc) => ({
 			pageName: (currentSearchablePage as string) ?? '',
@@ -22,17 +25,24 @@ async function getSearchableInPage(page: puppeteer.Page, currentSearchablePage: 
 	return foundContent;
 }
 
+function createIndexFile(scrappedData: IScrappedData[]) {
+	const options = { keys: ['name', 'text'] };
+	const indexer = Fuse.createIndex(options.keys, scrappedData);
+	fs.writeFileSync('indexer.json', JSON.stringify(indexer.toJSON(), null, '\t'));
+}
+
 async function createSearchIndex() {
 	const browser = await puppeteer.launch({ headless: isHeadless });
 	const page = await browser.newPage();
 	await page.goto(baseURL);
 
+	const scrappedData: IScrappedData[] = [];
 	for (const s of searchablePages) {
 		const found = await getSearchableInPage(page, s);
-		console.log('in page ' + s);
-		console.log(found);
-		console.log('========');
+		scrappedData.push(...found);
 	}
+
+	createIndexFile(scrappedData);
 
 	await browser.close();
 }
