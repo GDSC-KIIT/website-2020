5bd6484b9a7ce5aa7cf02fd4ae306e17838fe3ff
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-04-18 18:41:19 +0530

    remove unnecessary components from result items

--- src/components/Navbar/SearchBar/ResultItems.tsx
+++ src/components/Navbar/SearchBar/ResultItems.tsx
@@ -1,42 +1,41 @@
 import NextLink from 'next/link';
-import { Box, Typography, Grid, Theme, createStyles, makeStyles } from '@material-ui/core';
+import { Grid, Theme, makeStyles } from '@material-ui/core';
 
 import type { SearchResultType } from '@/types/index';
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		resultBox: {
-			cursor: 'pointer',
-			boxShadow: theme.shadows[1],
-			borderRadius: theme.shape.borderRadius,
-			backgroundColor: theme.palette.background.paper,
-			padding: theme.spacing(0.5),
-			width: theme.spacing(40),
-			maxHeight: theme.spacing(18),
-			'&:hover': {
-				backgroundColor: theme.palette.grey[200],
-			},
-			'&:focus': {
-				backgroundColor: theme.palette.action.selected,
-			},
+const useStyles = makeStyles((theme: Theme) => ({
+	resultBox: {
+		cursor: 'pointer',
+		boxShadow: theme.shadows[1],
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(0.5),
+		width: theme.spacing(40),
+		maxHeight: theme.spacing(18),
+		'&:hover': {
+			backgroundColor: theme.palette.grey[200],
 		},
-		heading: {
-			...theme.typography.subtitle1,
-			color: theme.palette.text.primary,
-			paddingLeft: theme.spacing(0.75),
-			fontWeight: theme.typography.fontWeightBold,
-			lineHeight: theme.spacing(0.15),
+		'&:focus': {
+			backgroundColor: theme.palette.action.selected,
 		},
-		text: {
-			...theme.typography.subtitle2,
-			color: theme.palette.text.secondary,
-			paddingLeft: theme.spacing(1),
-			textOverflow: 'ellipsis',
-			overflow: 'hidden',
-			maxHeight: theme.spacing(11),
-		},
-	})
-);
+	},
+	heading: {
+		...theme.typography.subtitle1,
+		color: theme.palette.text.primary,
+		margin: 0,
+		paddingLeft: theme.spacing(0.75),
+		fontWeight: theme.typography.fontWeightBold,
+		lineHeight: theme.spacing(0.15),
+	},
+	text: {
+		...theme.typography.subtitle2,
+		color: theme.palette.text.secondary,
+		paddingLeft: theme.spacing(1),
+		textOverflow: 'ellipsis',
+		overflow: 'hidden',
+		maxHeight: theme.spacing(11),
+	},
+}));
 
 export default function ResultItems({ results }: IResultItemProps) {
 	const classes = useStyles();
@@ -49,10 +48,10 @@ export default function ResultItems({ results }: IResultItemProps) {
 					prefetch={false}
 					scroll={false}>
 					<Grid item>
-						<Box className={classes.resultBox}>
-							<Typography className={classes.heading}>{r.item.name}</Typography>
+						<div className={classes.resultBox}>
+							<p className={classes.heading}>{r.item.name}</p>
 							<div className={classes.text}>{r.item.text}</div>
-						</Box>
+						</div>
 					</Grid>
 				</NextLink>
 			))}
